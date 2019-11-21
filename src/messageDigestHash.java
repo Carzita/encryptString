b55a4d07673bf5592335348585f739917e2df44e
Carzita <henrik.waersted@gmail.com>
2019-11-21 22:46:16 +0100

    updated with easy salt and hashed password encoding print

--- src/messageDigestHash.java
+++ src/messageDigestHash.java
@@ -1,20 +1,36 @@
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.security.spec.KeySpec;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class messageDigestHash {
 
     public static void main(String[] args) {
 
-        String stringToBeHashed = "this is to be hashed";
+        String stringToBeHashed = "password";
         System.out.println("String before hash: " + stringToBeHashed);
 
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
         random.nextBytes(salt);
+        System.out.println("salt: " + new String(salt, StandardCharsets.ISO_8859_1));
+/*        try (FileOutputStream stream = new FileOutputStream("salt.txt")) {
+            stream.write(salt);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        String encoded = Base64.getEncoder().encodeToString(salt);
+        System.out.println("salt hashed and then encoded: " + encoded);
         try {
 /*            MessageDigest mD = MessageDigest.getInstance("SHA-256");
             mD.update(stringToBeHashed.getBytes());
@@ -31,7 +47,10 @@ public class messageDigestHash {
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 
             byte[] hashedString = factory.generateSecret(spec).getEncoded();
-            System.out.println("Hashed string with salt: " + hashedString);
+            String password = Base64.getEncoder().encodeToString(hashedString);
+            System.out.println("encoded password: " + password);
+/*            System.out.println("Hashed string with salt: " + Arrays.toString(hashedString));
+            System.out.write(hashedString);*/
 
 
 
